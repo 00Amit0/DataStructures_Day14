fc8c37b61306c44042211b1f408b1cf59c55a553
“00Amit0” <amit836961@gmail.com>
2022-06-06 02:38:50 +0530

    Adding elements and displaying it

---
+++ LinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresDemo
+{
+    public class LinkedList
+    {
+        internal Node head;
+        /// <summary>
+        /// Adds the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        internal void Add(int data)                  //method to add elements in linked list
+        {
+            Node node = new Node(data);              //object of node to add element
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;                     //temp vatiable stores data
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresDemo
+{
+    public class Node
+    {
+        public int data;         //holds elements
+        public Node next;        //holds pointer position of next element
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Node"/> class.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
