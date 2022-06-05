10892a94546fde0d4d340485d35f5d6304e6b5e5
“00Amit0” <amit836961@gmail.com>
2022-06-06 02:32:03 +0530

    Welcome to Linked List

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DataStructuresDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Heyyy do u know what is linked list");
+
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+    }
+}
